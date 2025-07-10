7e1e2f6f9245ac729f9eb98591f23136e42829d1
kolhekar04 <kolhekaranshul9@gmail.com>
2025-07-10 22:53:10 +0530

    next_step_done

---
+++ demo.py
@@ -0,0 +1,13 @@
+# from src.logger import logging
+
+# logging.debug("this is demo message")
+
+from src.logger import logging
+from src.exception import MyException
+import sys
+
+try:
+    a = 1+'Z'
+except Exception as e:
+    logging.info(e)
+    raise MyException(e, sys) from e
