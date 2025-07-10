9e6de0ea2ba9d517bea0129f4dc1585d6ad9a60d
kolhekar04 <kolhekaranshul9@gmail.com>
2025-07-10 14:52:54 +0530

    structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anshul Kolhekar",
+    author_email="kolhekaranshul9@.com",
+    packages=find_packages()
+)
